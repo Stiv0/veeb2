dfdc6b514c693db3c3e5bbfdc292f3dea3e0e182
stivsire <stivsire@greeny.cs.tlu.ee>
2017-03-10 15:05:37 +0200

    sdasd

--- src/main/java/Stivo/Rakendus.java
+++ src/main/java/Stivo/Rakendus.java
@@ -17,6 +17,17 @@ public class Rakendus {
 		return arv1 +" ja "+ arv2 +" geomeetriline keskmine on "+ Math.sqrt(a1*a2);
 	}
 	
+	@RequestMapping("/geomeetrilineKeskmine3")
+	String gk(String arv1, String arv2){
+		if(arv1 == null){
+			return "Andmed puuduvad";
+		}
+		int a1 = Integer.parseInt(arv1);
+		int a2 = Integer.parseInt(arv2);
+		int a3 = Integer.parseInt(arv3);
+		return arv1 +", "+ arv2 +" ja "+ arv3 +" geomeetriline keskmine on "+ Math.pow(a1*a2*a3, (1/3));
+	}
+
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
         return "Ahoi!";
